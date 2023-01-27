db6accb43f52a6d86c068aa48652e4f4972c4b68
omognuni <melbong0069@gmail.com>
2023-01-27 12:32:43 +0900

    [Test] recipe api 테스트

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -21,7 +22,9 @@ class PrivateAPITest(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.client.force_authenticate()
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com', password='testpass')
+        self.client.force_authenticate(self.user)
 
     def test_retrieve_recipe(self):
         create_recipe()
@@ -33,26 +36,26 @@ class PrivateAPITest(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializer.data)
+        self.assertEqual(res.data['results'], serializer.data)
 
-    @patch('core.tasks.get_recipe')
-    @patch('core.tasks.get_recipe_url')
-    @patch('core.tasks.save_recipe')
-    def test_search_recipe(self, save_recipe, get_recipe_url, get_recipe):
+    @patch('celery.group.apply_async')
+    @patch('core.tasks.get_recipe_url.delay')
+    @patch('core.tasks.save_recipe.delay')
+    def test_search_recipe(self, save_recipe, get_recipe_url, group):
         recipe = create_recipe()
         tag = Tag.objects.create(name='test')
         Ingredient.objects.create(
             name='kimchi', number='1', unit='포기', recipe=recipe)
         recipe.tags.add(tag)
 
-        params = {'search': 'test'}
+        params = {'search': 'test', 'page': '1'}
         res = self.client.get(RECIPE_URL, params)
 
-        recipe = Recipe.objects.filter(tag__name='test')
-        serializer = RecipeSerializer(recipe)
+        recipe = Recipe.objects.filter(tags__name='test')
+        serializer = RecipeSerializer(recipe, many=True)
 
+        group.assert_called()
         get_recipe_url.assert_called()
-        get_recipe.assert_called()
         save_recipe.assert_called()
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.asertEqual(res.data, serializer.data)
+        self.assertEqual(res.data['results'], serializer.data)
