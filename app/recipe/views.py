b81a06973df9e3bfdce593d51c4e6e32ab5460ed
omognuni <melbong0069@gmail.com>
2024-02-24 23:21:13 +0900

    delete comments

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -39,22 +39,3 @@ class RecipeDetailView(APIView):
 
         output_serializer = self.serializer_class(recipe)
         return Response(output_serializer.data, status=status.HTTP_200_OK)
-
-
-#     def get_queryset(self):
-#         keyword = self.request.query_params.get('search')
-#         # 페이지 없이 검색할 시 기본값으로 1
-#         page = self.request.query_params.get('page', '1')
-#         if not page.isdigit():
-#             return
-#         page = int(page)
-#         if not keyword:
-#             return self.queryset
-#         queryset = self.queryset.filter(tags__name=keyword)
-#         # queryset 크기가 page*page_size보다 작을 경우 부족한 만큼의 페이지 가져오기
-#         start = len(queryset) // self.pagination_class.page_size
-#         if start >= page:
-#             return queryset
-#         self._run_celery_task(keyword, start, page)
-
-#         return self.queryset.filter(tags__name=keyword)
